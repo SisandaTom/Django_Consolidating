61b7392e421538c52efd3e5c5b6f83ebdcad666a
Anathi Swaphi <anathi@entersekt.com>
2023-09-06 15:55:04 +0200

    Add docstrings to models.py

--- Webpage/my_page/reviews/models.py
+++ Webpage/my_page/reviews/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 # Create your models here.
 
-# definig class Reviews to write syudent reviews for the website
+
 class Reviews(models.Model):
-# Default behaviour - Django creates primary keys for you
-# taking title, bosy and the student's name as input
+    """
+    Model representing student reviews for the website.
+
+    This model stores information about student reviews, including the title,
+    body, and the student's name.
+
+    Attributes:
+        title (str): The title of the review (max length: 140 characters).
+        body (str): The main content of the review.
+        name (str): The name of the student who wrote the review (max length: 140 characters).
+
+    Methods:
+        __str__(): Returns the title of the review as its string representation.
+
+    Usage:
+        You can use this model to store and retrieve student reviews for your website.
+    """
+
     title = models.CharField(max_length=140)
     body = models.TextField()
     name = models.CharField(max_length=140)
-def __str__(self):
-    return self.title
+
+    def __str__(self):
+        return self.title
